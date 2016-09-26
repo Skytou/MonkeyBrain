8dcce1744b9c04ea2f402cd5adc1ef917f0c823c
Srivatsan <srivatsan@skytousolution.com>
2016-09-26 16:50:24 +0530

    made Some Changes

---
+++ Assets/Scripts/Gravity2DTest.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity2DTest : MonoBehaviour {
+    Rigidbody2D monkeyRb;
+    public int gravityScale;
+    public bool gravityUp;
+    public bool gravityDown;
+    public bool gravityLeft;
+    public bool gravityRight;
+    public float moveSpeed;
+    // Use this for initialization
+    void Start ()
+    {
+        monkeyRb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GravityChange();
+	}
+
+    public void Gravity()
+    {
+        gravityScale += 1;
+        switch (gravityScale)
+        {
+            case 1:
+                gravityDown = true;
+                gravityLeft = false;
+                gravityRight = false;
+                gravityUp = false;
+
+                break;
+            case 2:
+
+                gravityDown = false;
+                gravityLeft = true;
+                gravityRight = false;
+                gravityUp = false;
+
+                break;
+            case 3:
+
+                gravityDown = false;
+                gravityLeft = false;
+                gravityRight = false;
+                gravityUp = true;
+                break;
+            case 4:
+
+                gravityDown = false;
+                gravityLeft = false;
+                gravityRight = true;
+                gravityUp = false;
+
+                break;
+
+
+        }
+        if (gravityScale > 3)
+        {
+            gravityScale = 0;
+        }
+    }
+    public void GravityChange()
+    {
+        if (gravityDown)
+        {
+            monkeyRb.AddForce(Vector2.down*moveSpeed);
+        }
+        if (gravityLeft)
+        {
+            monkeyRb.AddForce(Vector2.left*moveSpeed);
+        }
+        if (gravityUp)
+        {
+            monkeyRb.AddForce(Vector2.up*moveSpeed);
+        }
+        if (gravityRight)
+        {
+            monkeyRb.AddForce(Vector2.right*moveSpeed);
+        }
+    }
+}
